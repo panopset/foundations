5d228089afe443d62c8fec5ff7f2d8c3280590c6
Karl Dinwiddie <karl@panopset.com>
2016-04-17 22:00:05 -0700

    sound of silence is better

--- scripts/chrome/installChrome.sh
+++ scripts/chrome/installChrome.sh
@@ -10,5 +10,5 @@ sudo apt-get -y install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 #http://blog.manishchhabra.com/2012/05/installing-google-chrome-in-ubuntu-fix-error-for-missing-package-libcurl3-libnss3-1d-libxss1/
-sudo apt-get install -f
+sudo apt-get -y install -f
 
--- scripts/general/ubuntuUpdate.sh
+++ scripts/general/ubuntuUpdate.sh
@@ -7,7 +7,7 @@
 sudo apt-get -y update
 
 #http://askubuntu.com/questions/104899/make-apt-get-or-aptitude-run-with-y-but-not-prompt-for-replacement-of-configu
-sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+sudo apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 
 sudo apt-get -y upgrade
 
