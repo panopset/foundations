145c547fc5e1f6049b348a1d02bd3689357a1efa
Karl Dinwiddie <karl@panopset.com>
2016-07-09 06:44:19 -0700

    Add samba to pc profile.

--- profiles/pc/workstationMustHaves.sh
+++ profiles/pc/workstationMustHaves.sh
@@ -6,5 +6,5 @@
 #   Ubuntu programs you'll want for a workstation.
 #
 #############################################################################
-sudo apt-get -y install unetbootin extlinux lib32z1 lib32z1-dev build-essential mtp-tools mtpfs traceroute multiarch-support vim apache2 wget filezilla git gitk git-gui subversion ssh authbind cpu-checker linux-firmware-nonfree ant maven gradle npm
+sudo apt-get -y install unetbootin extlinux lib32z1 lib32z1-dev build-essential mtp-tools mtpfs traceroute multiarch-support vim apache2 wget filezilla git gitk git-gui subversion ssh authbind cpu-checker samba linux-firmware-nonfree ant maven gradle npm
 
