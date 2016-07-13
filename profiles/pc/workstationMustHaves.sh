471cb3c37cb9d58bfd00af3017597c1a5e87fe60
Karl Dinwiddie <karl@panopset.com>
2016-07-13 09:33:38 -0700

    droid sdk deps update

--- profiles/pc/workstationMustHaves.sh
+++ profiles/pc/workstationMustHaves.sh
@@ -6,11 +6,15 @@
 #   Ubuntu programs you'll want for a workstation.
 #
 #############################################################################
-sudo apt-get -y install unetbootin extlinux lib32z1 lib32z1-dev build-essential mtp-tools mtpfs 
+
+sudo apt-get -y install unetbootin extlinux build-essential mtp-tools mtpfs 
 sudo apt-get -y install traceroute multiarch-support vim 
 sudo apt-get -y install wget filezilla git gitk git-gui
 sudo apt-get -y install subversion ssh authbind cpu-checker samba 
 sudo apt-get -y install ant maven gradle npm
+
+# Android
+sudo apt-get -y install lib32z1 lib32z1-dev lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 
 # Mint/Ubuntu software that is not open source.
 sudo apt-get -y install linux-firmware-nonfree 
