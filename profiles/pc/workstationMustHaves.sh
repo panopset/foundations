8f058426eed49f05b23bde439c465c6fa5730ad6
Karl Dinwiddie <karl@panopset.com>
2016-07-13 05:47:07 -0700

    Readibility

--- profiles/pc/workstationMustHaves.sh
+++ profiles/pc/workstationMustHaves.sh
@@ -6,5 +6,14 @@
 #   Ubuntu programs you'll want for a workstation.
 #
 #############################################################################
-sudo apt-get -y install unetbootin extlinux lib32z1 lib32z1-dev build-essential mtp-tools mtpfs traceroute multiarch-support vim apache2 wget filezilla git gitk git-gui subversion ssh authbind cpu-checker samba linux-firmware-nonfree ant maven gradle npm
+sudo apt-get -y install unetbootin extlinux lib32z1 lib32z1-dev build-essential mtp-tools mtpfs 
+sudo apt-get -y install traceroute multiarch-support vim 
+sudo apt-get -y install wget filezilla git gitk git-gui
+sudo apt-get -y install subversion ssh authbind cpu-checker samba 
+sudo apt-get -y install ant maven gradle npm
 
+# Mint/Ubuntu software that is not open source.
+sudo apt-get -y install linux-firmware-nonfree 
+
+# Apache2 http server.
+sudo apt-get -y install apache2
