bd8c1901380769e71ed343cfc26d90aa0ef7a1c3
Karl Dinwiddie <karl@panopset.com>
2016-07-16 06:19:19 -0700

    Move jenkins to port 8081.

--- profiles/jenkins/installJenkins.sh
+++ profiles/jenkins/installJenkins.sh
@@ -6,7 +6,9 @@
 #############################################################################
 
 #############################################################################
-# Set up Jenkins on the default port 8080.
+# Set up Jenkins on port 8081, since we have tomcat on 8080.
 #############################################################################
 sudo apt-get -y install jenkins
+sudo sed -i 's/HTTP_PORT=8080/HTTP_PORT=8081/g' /etc/default/jenkins
+sudo service jenkins restart
 
