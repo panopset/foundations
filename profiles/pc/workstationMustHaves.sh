e890e0757392f45d69fd9bc26146932297572bfa
Karl Dinwiddie <karl@panopset.com>
2016-09-20 05:42:51 -0700

    Go back to Linux Mint 17.3.

--- profiles/pc/workstationMustHaves.sh
+++ profiles/pc/workstationMustHaves.sh
@@ -7,24 +7,26 @@
 #
 #############################################################################
 
-sudo apt -y install unetbootin extlinux build-essential mtp-tools mtpfs 
-sudo apt -y install traceroute multiarch-support vim 
-sudo apt -y install wget filezilla git gitk git-gui
-sudo apt -y install subversion ssh authbind cpu-checker samba 
-sudo apt -y install ant maven gradle npm
+sudo apt-get -y install unetbootin extlinux build-essential mtp-tools mtpfs 
+sudo apt-get -y install traceroute multiarch-support vim 
+sudo apt-get -y install wget filezilla git gitk git-gui
+sudo apt-get -y install subversion ssh authbind cpu-checker samba 
+sudo apt-get -y install ant maven gradle npm
 
 # Software that is not open source.
-sudo apt -y install linux-firmware-nonfree 
+sudo apt-get -y install linux-firmware-nonfree 
 
 # Android
 #https://developer.android.com/studio/install.html
 #http://askubuntu.com/questions/637113/unable-to-locate-package-lib32bz2-1-0
-sudo apt -y install lib32z1 lib32ncurses5 libbz2-1.0:i386 lib32stdc++6 gmtp
+sudo apt-get -y install lib32z1 lib32ncurses5 lib32bz2-1.0 libbz2-1.0:i386 lib32stdc++6 gmtp
 #... and you'll still need to do this manually, after android sdk is installed:
 #https://mike42.me/blog/how-to-edit-emulator-flags-in-android-studio.
 
 # Servers
-sudo apt -y install apache2 tomcat8
+sudo apt-get -y install apache2 tomcat8
 
-# AMD Drivers
-sudo apt -y install dh-modaliases execstack debhelper dkms
+# Git config
+git config --global push.default simple
+
+
